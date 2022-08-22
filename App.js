efbec260c49a6992da0f1e48539cd08cf51fdfaa
shubham <shubhampalia26@gmail.com>
2022-08-22 14:02:20 +0530

    removed commented code

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  //Commented code
   return (
     <View style={styles.container}>
       <Text>Opened up App.js to start working on your app!</Text>
